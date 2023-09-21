902c5f4b79de6cc04b72f72d6a02d1d1fe50e1bf
illuminationZ <maxkyhadsadaphone@gmail.com>
2023-09-21 16:23:10 +0700

    remove service unused

--- src/services/email-otp/email-otp.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { EmailOtpService } from './email-otp.service';
-
-@Controller('email-otp')
-export class EmailOtpController {
-    constructor(private readonly emailOtpService: EmailOtpService) {}
-}
